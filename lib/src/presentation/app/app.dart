dd558e730cd9d5ebeb7bb427460a259b76894918
abubakr1808 <yusupovabubakir368@gmail.com>
2025-01-25 12:24:30 -0500

    endi tugadi

--- lib/src/presentation/app/app.dart
+++ lib/src/presentation/app/app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:registratsiya_javlon_aka/src/presentation/index_screen_home/view/hoome_screen_entre_scren_nabbar.dart';
-// import 'package:registratsiya_javlon_aka/src/presentation/intro/view/splash_screen_view.dart';
+import 'package:registratsiya_javlon_aka/src/presentation/intro/view/splash_screen_view.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
 class App extends StatelessWidget {
@@ -11,8 +10,7 @@ class App extends StatelessWidget {
     return ResponsiveSizer(
       builder: (p0, p1, p2) => MaterialApp(
         debugShowCheckedModeBanner: false,
-        // home: SplashScreenView(),
-        home: HomeScreenEntreScreenNavbar(),
+        home: SplashScreenView(),
       ),
     );
   }
